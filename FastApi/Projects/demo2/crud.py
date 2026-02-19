b9bd21eb6c15a1b399e289ddc85c22adee092d7e
Samaresh <118683079+samaresh77@users.noreply.github.com>
2026-02-19 04:00:15 +0000

    crud

---
+++ FastApi/Projects/demo2/crud.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+import models, schemas
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(
+        name=user.name,
+        email=user.email,
+        age=user.age
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
