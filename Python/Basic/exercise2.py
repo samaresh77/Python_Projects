6806a0873cf865d83d7e18abe9a0d2d038b2e959
samaresh77 <samareshmondal53547@gmail.com>
2026-09-01 22:57:03 +0530

    ex2

---
+++ Python/Basic/exercise2.py
@@ -0,0 +1,14 @@
+n1 = int(input("Enter a number: "))
+n2 = int(input("Enter 2nd number: "))
+
+print(f"Addition: {n1+n2}")
+print(f"Subtraction: {n1-n2}")
+print(f"Multiplication: {n1*n2}")
+print(f"Division: {n1/n2}")
+
+# 
+name = "John"
+age = 30
+is_active = True
+
+print(f"User {name} is {age} years old.\nActive status: {is_active}")
