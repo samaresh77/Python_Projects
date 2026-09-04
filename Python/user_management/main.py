8a7ab16f539a7b3754fb27d8903bfc39b109f175
samaresh77 <samareshmondal53547@gmail.com>
2026-09-04 22:34:49 +0530

    updated exception

--- Python/user_management/main.py
+++ Python/user_management/main.py
@@ -1,29 +1,44 @@
 from users import add_user, view_users, search_user
 
+
 def main():
     while True:
-        # menu
         print(
             "\n1. Add User\n"
             "2. View Users\n"
             "3. Search User\n"
             "4. Exit"
         )
-        option = int(input("Choose option from 1 to 4: "))
+
+        try:
+            option = int(input("Choose option from 1 to 4: "))
+
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+
         if option == 1:
-            add_user()
+            try:
+                add_user()
+            except ValueError as error:
+                print("Error:", error)
 
         elif option == 2:
             view_users()
 
         elif option == 3:
-            search_user()
+            try:
+                search_user()
+            except ValueError as error:
+                print("Error:", error)
 
         elif option == 4:
             print("Goodbye!")
             break
+
         else:
             print("Invalid option")
+
 
 if __name__ == "__main__":
     main()
