cf978120e012752f4eb62f68ca6a4177d9cfae94
samaresh77 <samareshmondal53547@gmail.com>
2026-09-09 23:20:13 +0530

    venv

---
+++ Python/Professional_Python/venv.py
@@ -0,0 +1,8 @@
+# A virtual environment gives each project its own isolated packages:
+# Why do we need a virtual environment?
+
+# Creating a Virtual Environment
+# python3 -m venv .venv
+
+# Activating the Environment on Mac
+# source .venv/bin/activate
